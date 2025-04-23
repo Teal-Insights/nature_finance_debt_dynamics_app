b8c7a9376683ca6d3474450b4841fec087d2a681
Reu-hub <opondoreuben94@gmail.com>
2025-04-23 15:44:41 +0300

    improved the content in home tab

--- ui.R
+++ ui.R
@@ -21,8 +21,6 @@ source(file = "components/ui_footer_component.R")
 source(file = "components/ui_documentation_component.R")
 source(file = "components/ui_contact_component.R")
 # data: -------------------------------------------------------------------
-# df_countries <- imfweo::weo_list_countries() %>%
-#   rename(iso3c = "country_code", label = "country_name")
 df_countries <- imf_countries()
 # pickers: ----------------------------------------------------------------
 select_country <- df_countries %>%
@@ -57,48 +55,285 @@ ui <- bslib::page_navbar(
       )
     )
   ),
+  # CSS imports
   tags$head(
-    # icon part
+    # Favicon
     tags$link(rel = "icon", type = "image/x-icon", href = "ssdh_icon.png"),
-    # navbar css file
+    
+    # tabs
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/tab_home.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/tab_analysis.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/tab_data.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/documentation.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/tab_contact.css"),
+    
+    # navbar and footer
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/navbar.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/footer.css"),
+    
+    # integrated
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles/integrated.css"),
+    
+    # bootstrap
     tags$link(
-      rel = "stylesheet", type = "text/css",
-      href = "styles/navbar.css"
-    ),
-    # Integrated styles
-    tags$link(
-      rel = "stylesheet", type = "text/css",
-      href = "styles/integrated.css"
-    ),
-    # footer styles
-    tags$link(
-      rel = "stylesheet", type = "text/css",
-      href = "styles/footer.css"
-    ),
-    # documentation styles
-    tags$link(
-      rel = "stylesheet", type = "text/css",
-      href = "styles/documentation.css"
-    ),
-    # contact
-    tags$link(
-      rel = "stylesheet", type = "text/css",
-      href = "styles/tab_contact.css"
+      rel = "stylesheet", 
+      href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
     )
   ),
-  # adding space between title and other tabs
-  bslib::nav_spacer(),
+  
   # theme of the app
   theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
+  
+  # adding space between title and other tabs
+  bslib::nav_spacer(),
   
   # -------------------------------------------------------------------------
   # Home Panel
   # -------------------------------------------------------------------------
-  # bslib::nav_panel(
-  #   title = "Home",
-  #   bslib::card()
-  # ),
-
+  bslib::nav_panel(
+    title = "Home",
+    bslib::layout_column_wrap(
+      width = 1 / 2,
+      heights_equal = "row",
+      # About the FIMA Explorer App
+      bslib::card(
+        full_screen = TRUE,
+        height = 400,
+        class = "border-0 shadow-none",
+        bslib::card_header(h5("About the Debt Path Explorer App")),
+        bslib::card_body(
+          fillable = TRUE,
+          fill = TRUE,
+          # About the app with title on border
+          navset_tab(
+            nav_panel(
+              title = "About the app", 
+              tags$div(
+                class = "home-box-container", 
+                style = "position: relative; margin-top: 10px; padding-top: 1px;",
+                
+                # Title that will appear on the border
+                tags$div(class = "home-box-title", "About the app"),
+                
+                # Content box
+                tags$div(
+                  class = "home-box p-2 mb-0 border rounded steps",
+                  tags$h6("Welcome to the Debt Path Explorer (PROTOTYPE)", class = "top-header"),
+                  p(
+                    "
+                      This web application demonstrates how policy changes could 
+                      affect sovereign debt trajectories. It uses IMF data and 
+                      standard debt dynamics equations to project how different 
+                      macroeconomic factors influence debt sustainability. All 
+                      projections should be viewed as illustrative scenarios, 
+                      not forecasts.
+                    "
+                  ),
+                  
+                  # -------------------------------
+                  # Purpose of This Tool
+                  # -------------------------------
+                  
+                  tags$h6("Purpose of This Tool"),
+                  tags$ul(
+                    class = "green-bullets",
+                    tags$li(
+                      span("Explore how policy changes affect sovereign debt sustainability through interactive scenarios")
+                    ),
+                    tags$li(
+                      span("Visualize debt trajectories based on adjustments to key macroeconomic factors")
+                    ),
+                    tags$li(
+                      span("Understand debt dynamics using standard equations and IMF baseline data")
+                    ),
+                    tags$li(
+                      span("Create illustrative scenarios to support policy discussions and analysis")
+                    ),
+                    tags$li(
+                      span("Compare historical debt trends with projected outcomes based on different policy shocks")
+                    ),
+                    tags$li(
+                      span("Provide a transparent, accessible tool for evaluating debt sustainability across countries")
+                    )
+                  ),
+                  p(
+                    "
+                      This tool provides order-of-magnitude approximations as a 
+                      first step before detailed economic analysis on a 
+                      country-specific basis.
+                    " 
+                  ),
+                  p(
+                    "
+                      This is an early prototype - we welcome your feedback on 
+                      functionality, usability, and potential applications to 
+                      help guide future development.
+                    "
+                  )
+                )
+              )
+            ),
+            nav_panel(
+              title = "Guide on using the app", 
+              # home tab with title on border
+              tags$div(
+                class = "home-box-container", 
+                style = "position: relative; margin-top: 10px; padding-top: 1px;",
+                # Title that will appear on the border
+                tags$div(class = "home-box-title", "Home tab"),
+                
+                # Content box
+                tags$div(
+                  class = "home-box steps",
+                  # step 1
+                  h6("Step 1 : Select Country", class = "top-header"),
+                  p(
+                    "
+                      Under select country dropdown, select country of your choice.
+                    "
+                  )
+                )
+              ),
+              # Analysis tab box with title on border
+              tags$div(
+                class = "home-box-container", 
+                style = "position: relative; margin-top: 10px; padding-top: 1px;",
+                
+                # Title that will appear on the border
+                tags$div(class = "home-box-title", "Analysis tab"),
+                
+                # Content box
+                tags$div(
+                  class = "home-box steps",
+                  # step 2
+                  h6("Step 2 : Shock input", class = "top-header"),
+                  tags$ul(
+                    class = "green-bullets",
+                    tags$li(
+                      span(
+                        "Key indicators in debt projections are ",
+                        tags$b("Primary balance"), " , ",
+                        tags$b("Real effective interest rate"), " and ",
+                        tags$b("GDP growth. "),
+                        "All these indicators are in ", tags$b("%.")
+                      )
+                    ),
+                    tags$li(
+                      span(
+                        "
+                          Navigate to the Policy Shock column under each 
+                          indicator Look for ",tags$b("input boxes"),
+                          " to modify shock values.
+                        "
+                      )
+                    ),
+                    tags$li(
+                      span(
+                        "The ", tags$b("Policy-Adjusted Forecast"),
+                        " is the sum of the", tags$b("IMF WEO Baseline"),
+                        " values plus the user-provided ", tags$b("Policy Shock"),
+                        " values.", 
+                      )
+                    ),
+                    tags$li(
+                      span(
+                        " The ", tags$b("Policy-Adjusted Forecast"),
+                        "values are plugged into the debt dynamics equation to
+                         calculate the new debt path."
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      ),
+      
+      # -------------------------------
+      # Assessment
+      # -------------------------------
+      
+      bslib::card(
+        full_screen = TRUE,
+        height = 400,
+        class = "border-0 shadow-none",
+        bslib::card_header(h5("Assessment")),
+        bslib::card_body(
+          fillable = TRUE,
+          fill = TRUE,
+          #---------------
+          # select country
+          #---------------
+          tags$div(
+            class = "home-box-container", 
+            style = "position: relative; margin-top: 10px; padding-top: 1px;",
+            # Title that will appear on the border
+            tags$div(class = "home-box-title", "Country"),
+            # Content box
+            tags$div(
+              class = "home-box steps",
+              style = "padding-top: 10px; padding-bottom: 0px;", 
+              div(
+                style = "margin-top: 5px; margin-bottom: 5px;", 
+                shinyWidgets::pickerInput(
+                  inputId = "id_country",
+                  label = NULL,
+                  choices = select_country,
+                  options = shinyWidgets::pickerOptions(
+                    actionsBox = TRUE,
+                    size = 10,
+                    selectedTextFormat = "count > 3",
+                    liveSearch = TRUE,
+                    liveSearchStyle = "contains",
+                    liveSearchPlaceholder = "Select country...",
+                    title = "Select country..."
+                  ),
+                  multiple = FALSE,
+                  selected = NULL,
+                  width = "300px"
+                )
+              )
+            )
+          ),
+          # projections start in
+          conditionalPanel(
+            condition = "input.id_country !== null && input.id_country !== ''",
+            tags$div(
+              class = "home-box-container", 
+              style = "position: relative; margin-top: 10px; padding-top: 1px;",
+              
+              # Title that will appear on the border
+              tags$div(class = "home-box-title", "Projection start"),
+              
+              # Content box
+              tags$div(
+                class = "home-box steps",
+                style = "padding-top: 10px; padding-bottom: 0px;", 
+                div(
+                  style = "margin-top: 5px; margin-bottom: 5px;", 
+                  shinyWidgets::pickerInput(
+                    inputId = "projection_year",
+                    label = NULL,
+                    choices = projection_years,
+                    options = shinyWidgets::pickerOptions(
+                      actionsBox = TRUE,
+                      size = 10,
+                      selectedTextFormat = "count > 3",
+                      liveSearch = TRUE,
+                      liveSearchStyle = "contains",
+                      liveSearchPlaceholder = "Search year..."
+                    ),
+                    multiple = FALSE
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  ),
   # -------------------------------------------------------------------------
   # Analysis
   # -------------------------------------------------------------------------
@@ -109,79 +344,9 @@ ui <- bslib::page_navbar(
         sidebar = bslib::sidebar(
           # sidebar section background colour
           bg = "#2c3e50",
-          # Select Country
-          shinyWidgets::pickerInput(
-            inputId = "id_country",
-            label = "Select: Country",
-            choices = select_country,
-            selected = NULL,
-            options = shinyWidgets::pickerOptions(
-              actionsBox = TRUE,
-              size = 10,
-              selectedTextFormat = "count > 3",
-              liveSearch = TRUE,
-              liveSearchStyle = "contains",
-              liveSearchPlaceholder = "Search country..."
-            ),
-            multiple = FALSE
-          ),
-          hr(),
-          # projection year picker
-          shinyWidgets::pickerInput(
-            inputId = "projection_year",
-            label = "Projections start in",
-            choices = projection_years,
-            options = shinyWidgets::pickerOptions(
-              actionsBox = TRUE,
-              size = 10,
-              selectedTextFormat = "count > 3",
-              liveSearch = TRUE,
-              liveSearchStyle = "contains",
-              liveSearchPlaceholder = "Search year..."
-            ),
-            multiple = FALSE
-          ),
-          hr(),
-          # Guidance text with custom styling
-          tags$div(
-            class = "guide-box p-3 mb-3 border rounded",
-            tags$p(
-              h5(
-                icon("info-circle"),
-                "Overview:"
-              ),
-              class = "fw-bold"
-            ),
-            tags$ul(
-              tags$li(
-                "Key indicators in debt projections are ",
-                tags$b("Primary balance"), " , ",
-                tags$b("Real effective interest rate"), " and ",
-                tags$b("GDP growth. "),
-                "All these indicators are in ", tags$b("%.")
-              ),
-              tags$li(
-                "Navigate to the Policy Shock column under each indicator",
-                tags$br(),
-                tags$span(
-                  "Look for ",
-                  tags$b("input boxes"),
-                  " to modify shock values."
-                )
-              ),
-              tags$li(
-                "The ", tags$b("Policy-Adjusted Forecast"),
-                " is the sum of the",
-                tags$b("IMF WEO Baseline"),
-                " values plus the user-provided ", tags$b("Policy Shock"),
-                " values.", " The ", tags$b("Policy-Adjusted Forecast"),
-                "values are plugged into the debt dynamics equation to
-                calculate the new debt path."
-              )
-            )
-          ),
-          hr()
+          
         ),
+        # main content area
         bslib::layout_column_wrap(
           width = NULL,
           heights_equal = "row",
